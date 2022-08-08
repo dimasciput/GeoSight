85f87ebd73ac79261150feeaabbc4bae724293e2
= <meomancer@gmail.com>
2022-08-08 14:50:10 +0700

    Update harvester form

--- django_project/frontend/src/pages/Admin/Harvesters/HarvesterApiGeography/index.jsx
+++ django_project/frontend/src/pages/Admin/Harvesters/HarvesterApiGeography/index.jsx
@@ -1,8 +1,9 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 
 import { render } from '../../../../app';
 import { store } from '../../../../store/admin';
-import Harvesters from '../../Harvesters'
+import Harvesters from '../../Harvesters';
+import JsonSelector from '../../../../components/JsonSelector';
 
 import './style.scss';
 
@@ -11,7 +12,36 @@ import './style.scss';
  * ExposedAPI App
  */
 export default function HarvesterApiGeography() {
-  const [attributes, setAttributes] = useState(JSON.parse(JSON.stringify(attributesData)))
+  const [attributes, setAttributes] = useState(
+    JSON.parse(JSON.stringify(attributesData))
+  )
+  const [open, setOpen] = useState(false)
+  const [url, setUrl] = useState(null)
+  const [onload, setOnload] = useState(true)
+
+  // Update url when the attributes changed
+  useEffect(() => {
+    const newUrl = attributes.filter(attr => {
+      return attr.name === "api_url"
+    })[0]?.value
+
+    setUrl(newUrl)
+  }, [attributes])
+
+  // Show api response modal when url changed
+  useEffect(() => {
+    if (!onload) {
+      if (url) {
+        setOpen(true)
+      }
+    }
+    setOnload(false)
+  }, [url])
+
+  const setInputAttributes = (attributes) => {
+    setAttributes(attributes)
+    setOpen(false)
+  }
 
   return (
     <Fragment>
@@ -20,6 +50,10 @@ export default function HarvesterApiGeography() {
         is_harvester={true}
         attributes={attributes}
         setAttributes={setAttributes}/>
+      <JsonSelector
+        open={open} setOpen={setOpen} url={url}
+        inputAttributes={JSON.parse(JSON.stringify(attributes))}
+        setInputAttributes={setInputAttributes}/>
     </Fragment>
   );
 }
