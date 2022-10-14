bf48c336256a743f2f958c8d6c868bb497dbe65c
= <meomancer@gmail.com>
2022-10-14 09:08:31 +0700

    Remove Play Control (temporary)

---
+++ django_project/frontend/src/components/Dashboard/LeftPanel/GlobalDateSelector/PlayControl/index.jsx
@@ -0,0 +1,120 @@
+/* ==========================================================================
+   DATE SELECTOR
+   ========================================================================== */
+
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
+import StopIcon from '@mui/icons-material/Stop';
+import Slider from '@mui/material/Slider';
+import { Actions } from "../../../../../store/dashboard";
+import { ThemeButton } from "../../../../Elements/Button";
+import { allDataIsReady } from "../../../../../utils/indicators";
+
+import './style.scss';
+
+/***
+ * Play control
+ */
+export default function PlayControl({ dates, initDate }) {
+  const dispatch = useDispatch();
+  const selectedGlobalTime = useSelector(state => state.selectedGlobalTime);
+  const indicatorsData = useSelector(state => state.indicatorsData);
+
+  // Speed it 2 second for 0%
+  const minSpeed = 10; // second
+  const [speed, setSpeed] = useState(1);
+  const [nextIdx, setNextIdx] = useState(-1);
+
+  // Set play control
+  const [isPlay, setIsPlay] = useState(false);
+  const [isPause, setIsPause] = useState(false);
+
+  const idx = dates.indexOf(selectedGlobalTime)
+
+  /**
+   * Control functions
+   */
+  const play = () => {
+    setIsPlay(true)
+    setIsPause(false)
+    setNextIdx(-1)
+  }
+  const pause = () => {
+    setIsPlay(false)
+    setIsPause(true)
+    setNextIdx(-1)
+  }
+  const stop = () => {
+    setIsPlay(false)
+    setIsPause(false)
+    setNextIdx(-1)
+    dispatch(Actions.SelectedGlobalTime.change(initDate))
+  }
+
+  /**
+   * Change date if play
+   */
+  useEffect(() => {
+    if (isPlay) {
+      const speedTime = minSpeed * 1000 / speed;
+      setTimeout(function () {
+        let nextIdx;
+        if (idx === dates.length - 1) {
+          nextIdx = 0
+        } else {
+          nextIdx = idx + 1
+        }
+        setNextIdx(nextIdx)
+      }, speedTime);
+    }
+  }, [selectedGlobalTime, isPlay]);
+
+  /**
+   * If is play and nextIDX changed
+   */
+  useEffect(() => {
+    if (isPlay && nextIdx >= 0) {
+      let indicatorLayerData = []
+      if (currentIndicatorLayer.indicators) {
+        currentIndicatorLayer.indicators.map(indicatorLayer => {
+          indicatorLayerData.push(indicatorsData[indicatorLayer.id])
+        })
+        if (allDataIsReady(indicatorLayerData)) {
+          dispatch(Actions.SelectedGlobalTime.change(dates[nextIdx]))
+        }
+      }
+    }
+  }, [indicatorsData, nextIdx]);
+
+  return <div className='GlobalDateSelectionControl'>
+    <ThemeButton
+      variant={'secondary Basic ' + (!isPlay ? 'Reverse' : '')}
+      disabled={isPlay} onClick={play}>
+      <PlayArrowIcon/>
+    </ThemeButton>
+    <ThemeButton
+      variant={'secondary Basic ' + (!isPause ? 'Reverse' : '')}
+      disabled={!isPlay} onClick={pause}>
+      <PauseIcon/>
+    </ThemeButton>
+    <ThemeButton
+      variant={'secondary Basic Reverse'}
+      disabled={!isPlay && !isPause} onClick={stop}>
+      <StopIcon/>
+    </ThemeButton>
+    <div className='GlobalDateSelectionSpeedControl'>
+      <div className='GlobalDateSelectionSpeedControlText'>Speed
+      </div>
+      <Slider
+        value={speed}
+        min={1}
+        max={100}
+        onChange={(evt) => {
+          setSpeed(evt.target.value)
+        }}
+      />
+    </div>
+  </div>
+}
--- django_project/frontend/src/components/Dashboard/LeftPanel/GlobalDateSelector/index.jsx
+++ django_project/frontend/src/components/Dashboard/LeftPanel/GlobalDateSelector/index.jsx
@@ -9,9 +9,6 @@ import KeyboardDoubleArrowDownIcon
   from '@mui/icons-material/KeyboardDoubleArrowDown';
 import KeyboardDoubleArrowUpIcon
   from '@mui/icons-material/KeyboardDoubleArrowUp';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import PauseIcon from '@mui/icons-material/Pause';
-import StopIcon from '@mui/icons-material/Stop';
 import Slider from '@mui/material/Slider';
 
 import { fetchingData } from "../../../../Requests";
@@ -19,8 +16,7 @@ import { Actions } from "../../../../store/dashboard";
 import { formatDate } from "../../../../utils/main";
 
 import './style.scss';
-import { ThemeButton } from "../../../Elements/Button";
-import { allDataIsReady } from "../../../../utils/indicators";
+
 
 /**
  * Indicator data.
@@ -29,44 +25,11 @@ export default function GlobalDateSelector() {
   const dispatch = useDispatch();
   const { indicators } = useSelector(state => state.dashboard.data);
   const selectedGlobalTime = useSelector(state => state.selectedGlobalTime);
-  const indicatorsData = useSelector(state => state.indicatorsData);
-  const currentIndicatorLayer = useSelector(state => state.selectedIndicatorLayer);
 
   const [open, setOpen] = useState(false);
   const [initDate, setInitDate] = useState((new Date()).toISOString())
   const [datesByIndicators, setDatesByIndicators] = useState({})
   const [dates, setDates] = useState([initDate])
-
-  // Set play control
-  const [isPlay, setIsPlay] = useState(false);
-  const [isPause, setIsPause] = useState(false);
-
-  // Speed it 2 second for 0%
-  const minSpeed = 10; // second
-  const [speed, setSpeed] = useState(1);
-  const [nextIdx, setNextIdx] = useState(-1);
-
-  const idx = dates.indexOf(selectedGlobalTime)
-
-  /**
-   * Control functions
-   */
-  const play = () => {
-    setIsPlay(true)
-    setIsPause(false)
-    setNextIdx(-1)
-  }
-  const pause = () => {
-    setIsPlay(false)
-    setIsPause(true)
-    setNextIdx(-1)
-  }
-  const stop = () => {
-    setIsPlay(false)
-    setIsPause(false)
-    setNextIdx(-1)
-    dispatch(Actions.SelectedGlobalTime.change(initDate))
-  }
 
   /** Init date
    */
@@ -112,41 +75,6 @@ export default function GlobalDateSelector() {
     setDates([...newDates])
   }, [datesByIndicators]);
 
-  /**
-   * Change date if play
-   */
-  useEffect(() => {
-    if (isPlay) {
-      const speedTime = minSpeed * 1000 / speed;
-      setTimeout(function () {
-        let nextIdx;
-        if (idx === dates.length - 1) {
-          nextIdx = 0
-        } else {
-          nextIdx = idx + 1
-        }
-        setNextIdx(nextIdx)
-      }, speedTime);
-    }
-  }, [selectedGlobalTime, isPlay]);
-
-  /**
-   * If is play and nextIDX changed
-   */
-  useEffect(() => {
-    if (isPlay && nextIdx >= 0) {
-      let indicatorLayerData = []
-      if (currentIndicatorLayer.indicators) {
-        currentIndicatorLayer.indicators.map(indicatorLayer => {
-          indicatorLayerData.push(indicatorsData[indicatorLayer.id])
-        })
-        if (allDataIsReady(indicatorLayerData)) {
-          dispatch(Actions.SelectedGlobalTime.change(dates[nextIdx]))
-        }
-      }
-    }
-  }, [indicatorsData, nextIdx]);
-
   /***
    * Change the value text of the slider
    */
@@ -180,7 +108,6 @@ export default function GlobalDateSelector() {
                   max={dates.length - 1}
                   valueLabelFormat={valueLabelFormat}
                   step={null}
-                  disabled={isPlay}
                   valueLabelDisplay="auto"
                   marks={marks}
 
@@ -190,35 +117,6 @@ export default function GlobalDateSelector() {
                     )
                   }}
                 />
-              </div>
-              <div className='GlobalDateSelectionControl'>
-                <ThemeButton
-                  variant={'secondary Basic ' + (!isPlay ? 'Reverse' : '')}
-                  disabled={isPlay} onClick={play}>
-                  <PlayArrowIcon/>
-                </ThemeButton>
-                <ThemeButton
-                  variant={'secondary Basic ' + (!isPause ? 'Reverse' : '')}
-                  disabled={!isPlay} onClick={pause}>
-                  <PauseIcon/>
-                </ThemeButton>
-                <ThemeButton
-                  variant={'secondary Basic Reverse'}
-                  disabled={!isPlay && !isPause} onClick={stop}>
-                  <StopIcon/>
-                </ThemeButton>
-                <div className='GlobalDateSelectionSpeedControl'>
-                  <div className='GlobalDateSelectionSpeedControlText'>Speed
-                  </div>
-                  <Slider
-                    value={speed}
-                    min={1}
-                    max={100}
-                    onChange={(evt) => {
-                      setSpeed(evt.target.value)
-                    }}
-                  />
-                </div>
               </div>
             </div>
           </div>
